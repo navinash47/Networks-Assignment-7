b321bf62f784632536a9d68fa7add4934e86d869
Avinash Nandyala <navinash47@gmail.com>
2020-03-22 21:35:08 +0530

    basic includes for rsocket.h and some code in user1.c

--- rsocket.h
+++ rsocket.h
@@ -1,1 +1,16 @@
+#ifndef RSOCKET_H
+#define RSOCKET_H
 
+// Standard includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#endif
--- user1.c
+++ user1.c
@@ -1,1 +1,10 @@
+#include "rsocket.h"
 
+#define PORT1 53030
+#define PORT2 53031
+
+int main()
+{
+    char input[101];
+    printf("Enter String\n");
+}
