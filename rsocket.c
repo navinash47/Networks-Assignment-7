1ef7764fde6f75cb413a7f60d1e38204a949210e
Avinash Nandyala <navinash47@gmail.com>
2020-03-24 11:53:00 +0530

    intialized socket in function r_socket using udp socket

--- rsocket.c
+++ rsocket.c
@@ -1,1 +1,15 @@
+#include "rsocket.h"
 
+//initialize mutex locks
+pthread_t tid;
+pthread_mutex_t mutex;
+pthread_mutexattr_t mutex_attribute;
+int r_socket(int domain, int type, int protocol)
+{
+    int sockfd = socket(domain, SOCK_DGRAM, protocol);
+    if (sockfd < 0)
+    {
+        return -1;
+    }
+    return 0;
+}
