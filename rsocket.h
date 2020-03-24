82e1cfe39d9e5677d6d7dde075bb32ca2572bbbc
Avinash Nandyala <navinash47@gmail.com>
2020-03-24 10:33:16 +0530

    rsocket all functions filled

--- rsocket.h
+++ rsocket.h
@@ -35,5 +35,16 @@ typedef struct _unackbuff
 //function prototypes
 int r_socket(int domain, int type, int protocol);
 int r_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+ssize_t r_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+ssize_t r_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+void *thread_X(void *param);
+void HandleRetransmit(int sockfd);
+void HandleReceive(int sockfd, char *buffer, const struct sockaddr *src_addr, int msg_len);
+void HandleAppMsgRecv(int sockfd, char *buffer, const struct sockaddr *src_addr, int msg_len);
+void sendAck(int id, int sockfd, const struct sockaddr *dest_addr);
+void HandleACKMsgRecv(char *buffer);
+int min(int a, int b);
+int dropMessage(float p);
+int r_close(int sfd);
 
 #endif
